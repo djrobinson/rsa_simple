899cc6e3544646d83346781c109f405be24bc45d
Danny Robinson <drobinson@w-dn-ml-drobins.local>
2017-04-26 23:09:32 -0600

    notes

--- chat_server.js
+++ chat_server.js
@@ -4,9 +4,35 @@ const server = net.createServer();
 
 const sockets = [];
 
+//*********** Private/Public Key Setup ****************//
+
 const scrambler = (plainText) => {
     console.log(plainText.toUpperCase());
+    // Create 2 Prime Numbers
+
+
+    // Calculate n
+
+    // Calculate t
+
+
+    // Choose Prime Number e
+
+
+    // Find a d (Will require extra calc: http://www.pagedon.com/extended-euclidean-algorithm-in-c/my_programming/
+
+    )
 }
+
+const encrypt = (data) => {
+    // C = Me mod n
+}
+
+const decrypt = (data) => {
+    //  M = Cd mod n
+}
+
+//*****************************************************//
 
 server.on('connection', (socket) => {
     console.log('new connection');
