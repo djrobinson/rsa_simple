99b6379e8fd18ff32a97c769c37a067acd16ea45
Danny Robinson <drobinson@w-dn-ml-drobins.local>
2017-04-27 09:56:38 -0600

    Generating Primes complete

--- chat_server.js
+++ chat_server.js
@@ -5,24 +5,63 @@ const server = net.createServer();
 const sockets = [];
 
 //*********** Private/Public Key Setup ****************//
+const isPrime = (number) => {
+    for ( var i = 2; i < number; i++ ) {
+        if ( number % i === 0 ) {
+            return false;
+        }
+    }
+    return true;
+}
+
+const generateRelativePrime = (relator, max) => {
+    let random = 1;
+    while (relator % random === 0) {
+        random = generateRandomPrime(max);
+        console.log('Random Relative Test', random);
+    }
+    if (relator % random !== 0) {
+        return random;
+    }
+}
+
+const generateRandomPrime = (max) => {
+    // Start w/ a non-prime
+    let random = 4;
+    while (!isPrime(random)) {
+        random = Math.floor(Math.random() * max);
+        console.log('Random', random);
+    }
+    if (isPrime(random)) {
+        return random;
+    }
+}
+
 
 const scrambler = (plainText) => {
     console.log(plainText.toUpperCase());
     // Create 2 Prime Numbers
 
-
+    const p = generateRandomPrime(1000);
+    const q = generateRandomPrime(1000);
+    const isMore = Math.max(p, q);
     // Calculate n
+    const n = p * q;
 
     // Calculate t
-
+    const t = (p - 1) * (q - 1);
 
     // Choose Prime Number e
+    const e = generateRelativePrime(t, isMore);
+
+    console.log('P, Q, N, T, E:', p, q, n, t, e);
 
 
     // Find a d (Will require extra calc: http://www.pagedon.com/extended-euclidean-algorithm-in-c/my_programming/
 
-    )
 }
+
+scrambler('Howdy!');
 
 const encrypt = (data) => {
     // C = Me mod n
@@ -36,6 +75,8 @@ const decrypt = (data) => {
 
 server.on('connection', (socket) => {
     console.log('new connection');
+
+
 
     sockets.push(socket);
 
